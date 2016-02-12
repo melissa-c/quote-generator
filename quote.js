235aa1a772eb8b63fb76d4c6a75085d599219889
mel <dyslixec@hotmail.com>
2016-02-12 22:41:05 +1300

    set up basic layout & style

---
+++ quote.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+
+  var quotes = [
+    {
+      quote: "I'd take the awe of understanding over the awe of \
+              ignorance any day.",
+      author: "Douglas Adams"
+    },
+    {
+      quote: "I may not have gone where I intended to go, but I \
+              think I have ended up where I needed to be.",
+      author: "Douglas Adams"
+    },
+    {
+      quote: "I love deadlines. I love the whooshing noise they \
+              make as they go by.",
+      author: "Douglas Adams"
+    },
+    {
+      quote: "A learning experience is one of those things that \
+              says, 'You know that thing you just did? Don't do that'.",
+      author: "Douglas Adams"
+    },
+    {
+      quote: "One way to keep feeling young is to keep finding \
+              new layers of immaturity.",
+      author: "Alain de Botton"
+    },
+    {
+      quote: "Work only starts when the fear of doing nothing \
+              finally exceeds the fear of doing something badly.",
+      author: "Alain de Botton"
+    },
+    {
+      quote: "Looking older is a version of looking tired that \
+              can't be cured by sleep.",
+      author: "Alain de Botton"
+    },
+    {
+      quote: "The only people we can still believe are normal \
+              are those we don't know very well.",
+      author: "Alain de Botton"
+    },
+    {
+      quote: "I've learned that people will forget what you said, \
+              people will forget what you did, but people will never \
+              forget how you made them feel.",
+      author: "Maya Angelou" 
+    },
+    {
+      quote: "I have an inferiority complex, but it's not a very good one.",
+      author: "Steven Wright"
+    },
+    {
+      quote: "Right now I'm having amnesia and déjà vu at the same \
+              time - I think I've forgotten this before.",
+      author: "Steven Wright"
+    },
+    {
+      quote: "Self-love is a good thing but self-awareness is more \
+              important. You need to once in a while go, ‘Uh, I'm \
+              kind of an asshole'.",
+      author: "Louis C.K."
+    },
+    {
+      quote: "What happens after you die? Lot's of things happen \
+              after you die - they just don't involve you.",
+      author: "Louis C.K."
+    },
+    {
+      quote: "I'm in love with a philosophy major, and she doesn't \
+              even know I exist. And what's worse, she can prove it.",
+      author: "Arj Barker"
+    },
+    {
+      quote: "I just got a new iPod. It's got 80 gigabytes. \
+              Because I like to jog for three weeks at a time \
+              and I do not want to hear the same song twice.",
+      author: "Arj Barker"
+    },
+    {
+      quote: "I'm not even worried about settling down. I think \
+              it's way too early. I'm 25 and I'm in show business. \
+              I mean, if things go well, my wife hasn't even been born yet.",
+      author: "Arj Barker"
+    },
+    {
+      quote: "The good thing about science is that it's true whether \
+              or not you believe in it.",
+      author: "Neil deGrasse Tyson"
+    },
+    {
+      quote: "Knowing how to think empowers you far beyond those who \
+              know only what to think.",
+      author: "Neil deGrasse Tyson"
+    },
+    {
+      quote: "Not only do we live among the stars, the stars live within us.",
+      author: "Neil deGrasse Tyson"
+    },
+    {
+      quote: "All my friends are getting married. I guess I'm just at \
+              that age where people give up.",
+      author: "Amy Schumer"
+    },
+    {
+      quote: "I get labeled a sex comic. But if a guy got up onstage \
+              and pulled his dick out, everybody would say, \
+              ‘He's a thinker'.",
+      author: "Amy Schumer"
+    },
+    {
+      quote: "My ability to turn good news into anxiety is \
+              rivaled only by my ability to turn anxiety into chin acne.",
+      author: "Tina Fey"
+    },
+    {
+      quote: "However, if you do start crying in an argument and \
+              someone asks why, you can always say, 'I'm just crying \
+              because of how wrong you are'.",
+      author: "Amy Poeller"
+    },
+    {
+      quote: "I am against religion because it teaches us to be \
+              satisfied with not understanding the world.",
+      author: "Richard Dawkins"
+    },
+    {
+      quote: "Do not indoctrinate your children. Teach them how to \
+              think for themselves, how to evaluate evidence, and \
+              how to disagree with you.",
+      author: "Richard Dawkins"    
+    },
+  ]
+
+
+
+
+
+
+
+
+});
+
