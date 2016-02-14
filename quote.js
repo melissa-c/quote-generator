accdca2c3a2eac68937cce22cb95db999a894fa2
mel <dyslixec@hotmail.com>
2016-02-15 01:01:49 +1300

    add math random & twitter button

--- quote.js
+++ quote.js
@@ -1,144 +1,178 @@
+//for twitter button: opens new window
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
+
+// ------ start
 $(document).ready(function(){
 
   var quotes = [
     {
-      quote: "I'd take the awe of understanding over the awe of \
+      "quote": "I'd take the awe of understanding over the awe of \
               ignorance any day.",
-      author: "Douglas Adams"
+      "author": "Douglas Adams"
     },
     {
-      quote: "I may not have gone where I intended to go, but I \
+      "quote": "I may not have gone where I intended to go, but I \
               think I have ended up where I needed to be.",
-      author: "Douglas Adams"
+      "author": "Douglas Adams"
     },
     {
-      quote: "I love deadlines. I love the whooshing noise they \
+      "quote": "I love deadlines. I love the whooshing noise they \
               make as they go by.",
-      author: "Douglas Adams"
+      "author": "Douglas Adams"
     },
     {
-      quote: "A learning experience is one of those things that \
+      "quote": "A learning experience is one of those things that \
               says, 'You know that thing you just did? Don't do that'.",
-      author: "Douglas Adams"
+      "author": "Douglas Adams"
     },
     {
-      quote: "One way to keep feeling young is to keep finding \
+      "quote": "One way to keep feeling young is to keep finding \
               new layers of immaturity.",
-      author: "Alain de Botton"
+      "author": "Alain de Botton"
     },
     {
-      quote: "Work only starts when the fear of doing nothing \
+      "quote": "Work only starts when the fear of doing nothing \
               finally exceeds the fear of doing something badly.",
-      author: "Alain de Botton"
+      "author": "Alain de Botton"
     },
     {
-      quote: "Looking older is a version of looking tired that \
+      "quote": "Looking older is a version of looking tired that \
               can't be cured by sleep.",
-      author: "Alain de Botton"
+      "author": "Alain de Botton"
     },
     {
-      quote: "The only people we can still believe are normal \
+      "quote": "The only people we can still believe are normal \
               are those we don't know very well.",
-      author: "Alain de Botton"
+      "author": "Alain de Botton"
     },
     {
-      quote: "I've learned that people will forget what you said, \
+      "quote": "I've learned that people will forget what you said, \
               people will forget what you did, but people will never \
               forget how you made them feel.",
-      author: "Maya Angelou" 
+      "author": "Maya Angelou" 
     },
     {
-      quote: "I have an inferiority complex, but it's not a very good one.",
-      author: "Steven Wright"
+      "quote": "I have an inferiority complex, but it's not a very good one.",
+      "author": "Steven Wright"
     },
     {
-      quote: "Right now I'm having amnesia and déjà vu at the same \
+      "quote": "Right now I'm having amnesia and déjà vu at the same \
               time - I think I've forgotten this before.",
-      author: "Steven Wright"
+      "author": "Steven Wright"
     },
     {
-      quote: "Self-love is a good thing but self-awareness is more \
+      "quote": "Self-love is a good thing but self-awareness is more \
               important. You need to once in a while go, ‘Uh, I'm \
               kind of an asshole'.",
-      author: "Louis C.K."
+      "author": "Louis C.K."
     },
     {
-      quote: "What happens after you die? Lot's of things happen \
+      "quote": "What happens after you die? Lot's of things happen \
               after you die - they just don't involve you.",
-      author: "Louis C.K."
+      "author": "Louis C.K."
     },
     {
-      quote: "I'm in love with a philosophy major, and she doesn't \
+      "quote": "I'm in love with a philosophy major, and she doesn't \
               even know I exist. And what's worse, she can prove it.",
-      author: "Arj Barker"
+      "author": "Arj Barker"
     },
     {
-      quote: "I just got a new iPod. It's got 80 gigabytes. \
+      "quote": "I just got a new iPod. It's got 80 gigabytes. \
               Because I like to jog for three weeks at a time \
               and I do not want to hear the same song twice.",
-      author: "Arj Barker"
+      "author": "Arj Barker"
     },
     {
-      quote: "I'm not even worried about settling down. I think \
+      "quote": "I'm not even worried about settling down. I think \
               it's way too early. I'm 25 and I'm in show business. \
               I mean, if things go well, my wife hasn't even been born yet.",
-      author: "Arj Barker"
+      "author": "Arj Barker"
     },
     {
-      quote: "The good thing about science is that it's true whether \
+      "quote": "The good thing about science is that it's true whether \
               or not you believe in it.",
-      author: "Neil deGrasse Tyson"
+      "author": "Neil deGrasse Tyson"
     },
     {
-      quote: "Knowing how to think empowers you far beyond those who \
+      "quote": "Knowing how to think empowers you far beyond those who \
               know only what to think.",
-      author: "Neil deGrasse Tyson"
+      "author": "Neil deGrasse Tyson"
     },
     {
-      quote: "Not only do we live among the stars, the stars live within us.",
-      author: "Neil deGrasse Tyson"
+      "quote": "Not only do we live among the stars, the stars live within us.",
+      "author": "Neil deGrasse Tyson"
     },
     {
-      quote: "All my friends are getting married. I guess I'm just at \
+      "quote": "All my friends are getting married. I guess I'm just at \
               that age where people give up.",
-      author: "Amy Schumer"
+      "author": "Amy Schumer"
     },
     {
-      quote: "I get labeled a sex comic. But if a guy got up onstage \
+      "quote": "I get labeled a sex comic. But if a guy got up onstage \
               and pulled his dick out, everybody would say, \
               ‘He's a thinker'.",
-      author: "Amy Schumer"
+      "author": "Amy Schumer"
     },
     {
-      quote: "My ability to turn good news into anxiety is \
+      "quote": "My ability to turn good news into anxiety is \
               rivaled only by my ability to turn anxiety into chin acne.",
-      author: "Tina Fey"
+      "author": "Tina Fey"
     },
     {
-      quote: "However, if you do start crying in an argument and \
+      "quote": "However, if you do start crying in an argument and \
               someone asks why, you can always say, 'I'm just crying \
               because of how wrong you are'.",
-      author: "Amy Poeller"
+      "author": "Amy Poeller"
     },
     {
-      quote: "I am against religion because it teaches us to be \
+      "quote": "I am against religion because it teaches us to be \
               satisfied with not understanding the world.",
-      author: "Richard Dawkins"
+      "author": "Richard Dawkins"
     },
     {
-      quote: "Do not indoctrinate your children. Teach them how to \
+      "quote": "Do not indoctrinate your children. Teach them how to \
               think for themselves, how to evaluate evidence, and \
               how to disagree with you.",
-      author: "Richard Dawkins"    
-    },
+      "author": "Richard Dawkins"    
+    }
   ]
 
+  //button click event that uses math random to select set of 
+  //quote & author from the objects in the array.
 
-
-
-
-
-
-
+  $(".newQuote").click(function () {
+    function newQuote() {
+      var random = Math.floor(Math.random() * quotes.length);
+      $(".text").text(quotes[random].quote);
+      $(".author").text(quotes[random].author);
+    }
+    newQuote();
+  });
 });
 
+//twitter button not working properly - doesnt show quote.
+//and some quotes are over 140 characters....
+
+twttr.widgets.createShareButton(
+  'https://dev.twitter.com/',
+  document.getElementById('container'),
+  {
+    text: '#FavQuote'
+  }
+);
